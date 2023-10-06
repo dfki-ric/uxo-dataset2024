7a59d3b5ff18288b0a307f6b9bbb1c1cb1fb5f00
Nikolas Dahn <nikolas.dahn@dfki.de>
2023-10-06 18:26:33 +0200

    Added script specifically for downsampling

---
+++ 02_cut_gopro/downsample_all.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Downsampling takes a very, very long time for the 5.3k videos, so this is perfect for the cut clips
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <input-folder> <output-folder>"
+    exit 1
+fi
+
+INDIR=$1
+OUTDIR=$2
+OPTIONS='-vf scale=640:360 -c:v libx264 -an'
+
+mkdir -p "$OUTDIR"
+
+for file in "$INDIR"/*.mp4; do
+    echo "downsampling $file ..."
+    ffmpeg -loglevel error -stats -i "$file" $OPTIONS "$OUTDIR"/$(basename $file .mp4)_sd.mp4
+done
