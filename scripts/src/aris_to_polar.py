41aa98aeba6c32c6f30a10e0e7a73f94b6e7187d
Nikolas Dahn <nikolas.dahn@dfki.de>
2024-02-16 13:35:13 +0530

    Fixed rendering of polar sonar images

--- scripts/src/aris_to_polar.py
+++ scripts/src/aris_to_polar.py
@@ -5,40 +5,113 @@ import numpy as np
 import pandas as pd
 import cv2
 
-from aris_definitions import get_beamcount_from_pingmode, BeamWidthsAris3000_64, BeamWidthsAris3000_128
+from aris_definitions import get_beamcount_from_pingmode, BeamWidthsAris3000_64, BeamWidthsAris3000_128, FrameHeaderFields
 
 
 def usage():
     print(f'{sys.argv[0]} <input-folder> <output-folder>')
 
 
-def aris_frame_to_polar(frame, frame_idx, metadata):
+def aris_frame_to_polar(frame, frame_idx, metadata, norm_intensity=True, interpolate=True, scale=1.):
     frame_meta = metadata.iloc[frame_idx]
-    pingmode = frame_meta['PingMode']
-    bin_count = int(frame_meta['SamplesPerBeam'])
+    pingmode = frame_meta[FrameHeaderFields.ping_mode]
+    bin_count = int(frame_meta[FrameHeaderFields.samples_per_beam])
     beam_count = get_beamcount_from_pingmode(pingmode)
     
     if beam_count == 64:
-        beam_angles = BeamWidthsAris3000_64
+        beam_angles = np.array(BeamWidthsAris3000_64)
     elif beam_count == 128:
-        beam_angles = BeamWidthsAris3000_128
+        beam_angles = np.array(BeamWidthsAris3000_128)
     else:
         raise ValueError(f'Unexpected pingmode {pingmode}')
     
-    beam_range = beam_angles[-1][2] - beam_angles[0][1]
-    frame_half_w = int(np.ceil(bin_count * np.tan(np.deg2rad(beam_range / 2))))
-    polar_frame = np.zeros((bin_count, frame_half_w * 2), dtype=np.uint8)
+    window_start = frame_meta[FrameHeaderFields.window_start]
+    window_length = frame_meta[FrameHeaderFields.window_length]
+    bins_per_meter = bin_count / window_length
+    #beam_widths = beam_angles[:, 2] - beam_angles[:, 1]
+    
+    # The sonar only takes measurements starting from a certain distance
+    min_radius = bins_per_meter * window_start
+    
+    # The leftmost edge will be lower than the center of the closest profile
+    distance_bottom_left = np.sin(np.deg2rad(beam_angles[-1, 1] + 90)) * min_radius
+    
+    # We need a few additional pixel to include the corners of the bottom profile
+    bottom_profile_offset = int(abs(min_radius - distance_bottom_left))
+    
+    # The top profile is further away from the origin than the image bottom
+    last_profile_distance = int(bin_count + bottom_profile_offset + min_radius)
+    beam_range = beam_angles[-1, 2] - beam_angles[0, 1]
+    frame_half_w = int(np.ceil(last_profile_distance * np.tan(np.deg2rad(beam_range / 2))) * scale)
+    frame_h = int((bin_count + bottom_profile_offset) * scale)
+    polar_frame = np.zeros((frame_h, frame_half_w * 2), dtype=np.uint8)
+    
+    # The center of the circle is min_radius bins below the image bottom
+    center_y = int((bin_count + min_radius) * scale)
+    
+    if norm_intensity:
+        int_min = np.min(frame)
+        int_max = np.max(frame)
     
     for beam_idx in range(beam_count):
-        start_angle = beam_angles[beam_idx][1]
-        end_angle = beam_angles[beam_idx][2]
+        _, start_angle, end_angle = beam_angles[beam_idx]
         
         for bin_idx in range(bin_count):
             intensity = frame[bin_idx, beam_idx]
-            cv2.ellipse(polar_frame, (frame_half_w, bin_count), (bin_idx, bin_idx), 0, 270 + start_angle, 270 + end_angle, int(intensity), 1)
-        
+            
+            if norm_intensity:
+                intensity = int((intensity - int_min) / (int_max - int_min) * 255)
+            
+            # The radius of each circle has to account for the offset of the center and the bottom
+            # profile corners
+            radius = (bin_idx + min_radius + bottom_profile_offset) * scale
+            
+            # Originally this used cv2.ellipse; however, this gives terrible results as the angles 
+            # are internally rounded to integers so that a lookup table can be used. This uses a LOT
+            # of information in the resulting image!
+            
+            # Find out how many pixels across we need to travel (usually only one)
+            x_start = int(np.cos(np.deg2rad(start_angle + 90)) * radius + frame_half_w)
+            x_end = int(np.cos(np.deg2rad(end_angle + 90)) * radius + frame_half_w)
+            cross_distance = max(1, x_end - x_start + 1)
+            angle_delta = abs(end_angle - start_angle) / cross_distance
+            
+            for i in range(cross_distance):
+                angle = start_angle + i * angle_delta
+                x = int(np.cos(np.deg2rad(angle + 90)) * radius + frame_half_w)
+                y = int(frame_h - max(0, np.sin(np.deg2rad(angle + 90)) * radius - min_radius) - 1)
+                polar_frame[y, x] = intensity
+                
     # In ARIS frames, beams are ordered right to left
-    return cv2.flip(polar_frame, 1)
+    polar_frame = cv2.flip(polar_frame, 1)
+                
+    if interpolate:
+        mask = np.zeros(polar_frame.shape, dtype=np.uint8)
+        mask[polar_frame == 0] = 1
+        
+        sonar_left_corner = int(np.cos(np.deg2rad(beam_angles[-1, 2] + 90)) * min_radius + frame_half_w)
+        left_rect = np.array([[
+            (0, 0), 
+            (0, frame_h), 
+            (sonar_left_corner, frame_h),
+        ]])
+        cv2.fillPoly(mask, left_rect, 0)
+        
+        sonar_right_corner = int(np.cos(np.deg2rad(beam_angles[0, 1] + 90)) * min_radius + frame_half_w)
+        right_rect = np.array([[
+            (frame_half_w * 2, 0), 
+            (sonar_right_corner, frame_h),
+            (frame_half_w * 2, frame_h), 
+        ]])
+        cv2.fillPoly(mask, right_rect, 0)
+        
+        cv2.ellipse(mask, (frame_half_w, center_y), (int(min_radius), int(min_radius)), 0, 0, 360, 0 -1)
+        cv2.ellipse(mask, (frame_half_w, center_y), (frame_h + int(min_radius), frame_h + int(min_radius)), 0, 0, 360, 0, 10)
+        
+        polar_frame = cv2.inpaint(polar_frame, mask, 3, cv2.INPAINT_TELEA)
+        print('ping')
+        
+    return polar_frame
 
 
 if __name__ == '__main__':
