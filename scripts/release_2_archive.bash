74cd02ac2e624287951f879f6dc34fc3a6fbf38a
Nikolas Dahn <nikolas.dahn@dfki.de>
2024-04-24 17:01:16 +0200

    Archiving now splits the dataset in a more granular way

--- scripts/release_2_archive.bash
+++ scripts/release_2_archive.bash
@@ -4,15 +4,21 @@
 
 mydir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 indir=$(sed -n -e 's/^export_dir://p' $mydir/config.yaml | tr -d '"' | xargs)
-archive="$(dirname $indir)/$(basename $indir)"
+archive="$(dirname $indir)/archives/$(basename $indir)"
 
 trap "exit" INT
+mkdir -p "$(dirname $indir)/archives/"
 
-# Archive everything, excluding polar transformed images
-7z a -mx=5 -r "${archive}.7z" $indir -xr'!*/aris_polar/*'
+# Recordings, excluding polar transformed images
+7z a -mx=5 -r "${archive}_recordings.7z" $indir/recordings/ -xr'!*/aris_polar/*'
 
-# Archive only the polar transformed images
-# the xargs above makes sure that $indir doesn't contain any whitespaces
+# Only the polar transformed images
 7z a -mx=5 -r "${archive}_polar.7z" -ir'!'"${$indir}/*/aris_polar/*"
+
+# 3d models
+7z a -mx=5 -r "${archive}_3dmodels.7z" $indir/3d_models/
+
+# Scripts
+7z a -mx=5 -r "${archive}_scripts.7z" $indir/scripts/
 
 echo "Created archive: $archive"
