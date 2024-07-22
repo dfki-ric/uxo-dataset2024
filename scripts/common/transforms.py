6476dff7375a9d7aad007cfc1c3c3d8f5bab3c2f
mbandefirvida <miguel.bande_firvida@dfki.de>
2024-07-22 12:26:25 +0200

    added ploting of target

--- scripts/common/transforms.py
+++ scripts/common/transforms.py
@@ -13,9 +13,12 @@ def _read_yaml_file(filename):
     file = open(filename, 'r')
     return yaml.safe_load(file)
 
-def _generate_tf_manager(tf_statics):
-    tm = TransformManager()
-    for tf in tf_statics['setup']:
+
+def _generate_tf_manager(tf_statics, object='setup', tm=None):
+    if not tm:
+        tm = TransformManager()
+
+    for tf in tf_statics[object]:
         transform = pt.transform_from_pq(
                         np.hstack((
                             np.array([tf['translation']['x'], tf['translation']['y'], tf['translation']['z']]), 
@@ -26,11 +29,18 @@ def _generate_tf_manager(tf_statics):
     return tm 
 
 
+def _generate_tf_manager_for_all(tf_statics):
+    tm = TransformManager()
+    _generate_tf_manager(tf_statics, object='setup', tm=tm)
+    _generate_tf_manager(tf_statics, object='targets', tm=tm)
+    return tm
+
+
 def get_tf_manager():
-    filename = path.join(path.dirname(path.realpath(__file__)), 'tf_setup.yaml')
+    filename = path.join(path.dirname(path.realpath(__file__)), 'tf_statics.yaml')
     if path.isfile(filename):
         tf_statics = _read_yaml_file(filename)
-        tf_manager = _generate_tf_manager(tf_statics)
+        tf_manager = _generate_tf_manager_for_all(tf_statics)
         return tf_manager
     else:
         print(f"Tf file '{filename}' not found, returning an empty tf manager")
